e69324c98701cd3125a471da2d3e38b913238012
Lee seung chan <dltmdcks702@gmail.com>
2023-06-18 07:17:58 +0900

    init flutter

---
+++ android/app/src/main/kotlin/com/example/mundeuk_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.mundeuk_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:mundeuk_app/web_view_stack.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      theme: ThemeData(useMaterial3: true),
+      home: const WebViewApp(),
+    ),
+  );
+}
+
+class WebViewApp extends StatefulWidget {
+  const WebViewApp({super.key});
+
+  @override
+  State<WebViewApp> createState() => _WebViewAppState();
+}
+
+class _WebViewAppState extends State<WebViewApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: const WebViewStack(),
+    );
+  }
+}
