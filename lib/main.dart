43715ecddda32510e387e63712a1783ea6f94d7f
Lee seung chan <dltmdcks702@gmail.com>
2023-07-17 21:08:54 +0900

    init layout tutorial

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,8 @@ import 'dart:io';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_inappwebview/flutter_inappwebview.dart';
+import 'package:kakao_flutter_sdk/kakao_flutter_sdk.dart';
+import 'package:mundeuk_app/pages/login_page.dart';
 import 'package:mundeuk_app/permission_view.dart';
 import 'package:mundeuk_app/web_view_stack.dart';
 import 'package:permission_handler/permission_handler.dart';
@@ -17,12 +19,18 @@ Future<void> main() async {
     await AndroidInAppWebViewController.setWebContentsDebuggingEnabled(true);
   }
 
+  KakaoSdk.init(
+    nativeAppKey: const String.fromEnvironment("YOUR_NATIVE_APP_KEY"),
+    javaScriptAppKey: const String.fromEnvironment("YOUR_JAVASCRIPT_APP_KEY"),
+  );
+
   runApp(
     MaterialApp(
         theme: ThemeData(useMaterial3: true),
         initialRoute: '/',
         routes: {
-          '/': (context) => WebViewStack(context),
+          '/': (context) => const LoginPage(),
+          '/webview': (context) => WebViewStack(context),
           '/permission': (context) => PermissionViewStack(context),
         }),
   );
---
+++ lib/pages/login_page.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:kakao_flutter_sdk/kakao_flutter_sdk.dart';
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Colors.grey[300],
+        body: SafeArea(
+            child: Center(
+                child: Column(children: [
+          SizedBox(height: 50),
+          // logo
+          Icon(Icons.lock, size: 100),
+          SizedBox(height: 50),
+          // welcom message
+          Text("Welcome back you've been missed"),
+          // usename
+          TextButton(
+              onPressed: () async {
+                await loginWithKaKao();
+              },
+              child: Text('kakao login'))
+          // password
+        ]))));
+  }
+
+  Future<void> loginWithKaKao() async {
+    // 카카오톡 설치 여부 확인
+    // 카카오톡이 설치되어 있으면 카카오톡으로 로그인, 아니면 카카오계정으로 로그인
+    if (await isKakaoTalkInstalled()) {
+      try {
+        await UserApi.instance.loginWithKakaoTalk();
+        print('카카오톡으로 로그인 성공');
+      } catch (error) {
+        print('카카오톡으로 로그인 실패 $error');
+
+        // 사용자가 카카오톡 설치 후 디바이스 권한 요청 화면에서 로그인을 취소한 경우,
+        // 의도적인 로그인 취소로 보고 카카오계정으로 로그인 시도 없이 로그인 취소로 처리 (예: 뒤로 가기)
+        if (error is PlatformException && error.code == 'CANCELED') {
+          return;
+        }
+        // 카카오톡에 연결된 카카오계정이 없는 경우, 카카오계정으로 로그인
+        try {
+          await UserApi.instance.loginWithKakaoAccount();
+          print('카카오계정으로 로그인 성공');
+        } catch (error) {
+          print('카카오계정으로 로그인 실패 $error');
+        }
+      }
+    } else {
+      try {
+        await UserApi.instance.loginWithKakaoAccount();
+        print('카카오계정으로 로그인 성공');
+      } catch (error) {
+        print('카카오계정으로 로그인 실패 $error');
+      }
+    }
+  }
+}
